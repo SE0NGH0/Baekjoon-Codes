57c353644b632a32ff425663fa3ede2c014a094b
SE0NGH0 <140130211+SE0NGH0@users.noreply.github.com>
2023-09-26 10:56:07 +0900

    Update 점의 위치 구하기.c

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.c
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.c
@@ -6,25 +6,24 @@
 int solution(int dot[], size_t dot_len) {
     int answer = 0;
     
-    for (int i = 0; i < dot_len; i++)
+    
+    if (dot[0] > 0 && dot[1] > 0)
     {
-        if (dot[0] > 0 && dot[1] > 0)
-        {
-            answer = 1;
-        }
-        else if (dot[0] < 0 && dot[1] > 0)
-        {
-            answer = 2;
-        }
-        else if (dot[0] < 0 && dot[1] < 0)
-        {
-            answer = 3;
-        }
-        else if (dot[0] > 0 && dot[1] < 0)
-        {
-            answer = 4;
-        }
+        answer = 1;
     }
+    else if (dot[0] < 0 && dot[1] > 0)
+    {
+        answer = 2;
+    }
+    else if (dot[0] < 0 && dot[1] < 0)
+    {
+        answer = 3;
+    }
+    else if (dot[0] > 0 && dot[1] < 0)
+    {
+        answer = 4;
+    }
+ 
     
     return answer;
 }
