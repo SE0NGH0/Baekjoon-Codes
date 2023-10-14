ea6b90934a2f3ed66f4723f3aad2d7df7e31789c
SE0NGH0 <140130211+SE0NGH0@users.noreply.github.com>
2023-10-14 22:25:53 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.py
@@ -0,0 +1,12 @@
+def solution(num, k):
+    answer = 0
+    
+    for i in str(num):
+        answer += 1
+        
+        if i == str(k):
+            return answer
+        
+    return -1
+    
+    return answer
